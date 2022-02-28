72dd7ff714a51267fd2c6f00e23a24b6b5ab092a
Nikita Skulyberdin <nikitaskul2012@gmail.com>
2022-02-28 18:15:34 +0300

    Delete src directory

--- src/main/java/App/com/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package App.com.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import App.com.model.Student;
-
-public interface StudentRepository extends CrudRepository<Student, Long> {
-    
-}
